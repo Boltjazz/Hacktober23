c42a3598d8a4fc0b1d09dbdddc03208a515c6697
Yash <boltjazz10@gmail.com>
2023-10-01 23:22:24 +0530

    task

---
+++ Task2/Comp_coding/1_Count_Common_Factor.py
@@ -0,0 +1,13 @@
+#This problem is asked in one of the HackerEarth contests.
+
+#Problem Statement: Little Robert likes mathematics. Today his teacher has given him two integers and asked him to find out how many integers can divide both the numbers. Would you like to help him in completing his school assignment?
+
+#Input Formatting: There are two integers, a and b as input to the program.
+
+#Output Formatting: Print the number of common factors of a and b. Both the input value should be in a range of 1 to 10^12.
+
+#Example:
+
+#Input: 10 15
+#Output: 2
+#-------------------------------------------------
