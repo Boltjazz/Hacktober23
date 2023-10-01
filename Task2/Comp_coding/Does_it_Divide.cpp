ab4d43c58bbd58c5ae537c7fcc52e8b7be6300c9
Bharath-KumarReddy <127650446+Bharath-KumarReddy@users.noreply.github.com>
2023-10-01 23:57:35 +0530

    Update Does_it_Divide.cpp

--- Task2/Comp_coding/Does_it_Divide.cpp
+++ Task2/Comp_coding/Does_it_Divide.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 
 int main() {
-    int T;
-    std::cin >> T; 
+    int T; // Number of test cases
+    std::cin >> T; // Input the number of test cases
 
-    while (T--) {
-        int N;
-        std::cin >> N;
+    while (T--) { // Loop through each test case
+        int N; // The value for each test case
+        std::cin >> N; // Input the value
 
+        long long sum = 0; // Initialize a variable to store the sum
+        long long prod = 1; // Initialize a variable to store the product
 
-        long long sum = 0;
-        long long prod = 1;
-
+        // Calculate the sum and product from 1 to N
         for (int i = 1; i <= N; ++i) {
-            sum += i;
-            prod *= i;
+            sum += i; // Add i to the sum
+            prod *= i; // Multiply i to the product
         }
 
-
+        // Check if the product is divisible by the sum
         if (prod % sum == 0) {
-            std::cout << "YEAH" << std::endl;
+            std::cout << "YEAH" << std::endl; // If divisible, print "YEAH"
         } else {
-            std::cout << "NAH" << std::endl;
+            std::cout << "NAH" << std::endl; // If not divisible, print "NAH"
         }
     }
 
-    return 0;
+    return 0; // Exit the program
 }
