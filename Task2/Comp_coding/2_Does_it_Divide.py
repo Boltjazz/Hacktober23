f5cde848ab88c749a8531f16c5d8bb02d3a7f094
Aditya Gupta <adityagupta11219@gmail.com>
2023-10-02 02:10:06 +0530

    Added: solution for the morgan stanley problem

--- Task2/Comp_coding/2_Does_it_Divide.py
+++ Task2/Comp_coding/2_Does_it_Divide.py
@@ -1,32 +1,21 @@
-from math import sqrt
-def isPrime(n):
-	
-	
-	if (n <= 1):
-		return False
-	if (n <= 3):
-		return True
+Problem Statement: Consider a permutation of numbers from 1 to N written on a paper. Let’s denote the product of its element as ‘prod’ and the sum of its elements as ‘sum’. Given a positive integer N, your task is to determine whether ‘prod’ is divisible by ‘sum’ or not.
 
-	if (n % 2 == 0 or n % 3 == 0):
-		return False
+Input Format: First input will be an integer T.  It depicts a number of test cases. Followed by the value for each test case. Each test case will contain an integer N (1<= N <=10^9). It is nothing but the length of the permutation.
 
-	for i in range(5, int(sqrt(n)) + 1, 6):
-		if (n % i == 0 or n % (i + 2) == 0):
-			return False
+Output Format: For each test case, print “YEAH” if ‘prod’ is divisible by ‘sum’, otherwise print “NAH”.
 
-	return True
+Input:
 
-def isDivisible(n):
-	if (isPrime(n + 1)):
-		return False
-	return True
+2 
+2 
+3
+
+Output:
+
+YEAH
+NAH
 
 
-
-t = int(input())
-while t>0 :
-    n = int(input())
-    if(isDivisible(n)):
-        print("Yes")
-    else:
-        print("No")
+"You can solve the code in any language you want just change the extenshion of the file and make sure it run , you can the
+commit the code"
+-------------------------------------------------------------------------------------------------------------------------
