50f3d41771456b355e4019aedf7ada1f4aedf570
theharism <85572852+theharism@users.noreply.github.com>
2023-10-03 01:47:55 +0500

    Hacking Wizards

--- gitfetch.js
+++ gitfetch.js
@@ -84,6 +84,7 @@ const profiles = [
   { githubUsername: "vaibhavsahu1290", linkedinID: "vaibhav-sahu-ba5a8528b" },
   { githubUsername: "Rimjhim-mm", linkedinID: "rimjhim-tiwari-a391a7253" },
   { githubUsername: "Ayus3h", linkedinID: "ayush-yadav-57134728b" },
+  { githubUsername: "theharism", linkedinID: "muhammad-haris-0b6a6b1b7" },
 
   // Add more profiles as needed
 
